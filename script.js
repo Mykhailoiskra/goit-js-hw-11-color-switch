2c2d5ed9f2f824db827b98dc2c62bb74f0628e0a
Mykhailo Iskra <iskramichel@gmail.com>
2020-10-28 21:55:36 +0200

    Готовая к сдаче версия

--- script.js
+++ script.js
@@ -4,12 +4,31 @@ const colors = [
   '#4CAF50',
   '#FF9800',
   '#009688',
-'#795548','#a7e740', '#4091e7'
+  '#795548',
+  '#a7e740',
+  '#4091e7'
 ];
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
+const startBtn = document.querySelector('button[data-action="start"]');
+const endBtn = document.querySelector('button[data-action="stop"]');
+let timerId = null;
 
 
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(bodyColorChange, 1000);
+  startBtn.setAttribute('disabled', 'true');
+});
+endBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+});
+
+
+function bodyColorChange() {
+  const i = randomIntegerFromInterval(0, colors.length - 1);
+  document.body.style.backgroundColor = `${colors[i]}`;
+}
